a4310f695bdd4db0c74fb6f48535acb1d56e999d
Sourabh9879 <kumarsourabh970@gmail.com>
2024-08-01 21:52:52 +0530

    added authentication using passport.js

---
+++ auth.js
@@ -0,0 +1,22 @@
+const LocalStrategy = require('passport-local').Strategy;
+const Person = require('./models/Person');
+const passport = require('passport');
+
+passport.use(new LocalStrategy(async (USERNAME , password , done) => {
+    try {
+      console.log('recevied credentials', USERNAME, password);
+      const user = await Person.findOne({username :USERNAME})
+      if (!user) return done(null, false, { message: 'Incorrect username.' });
+  
+      const isPasswordMatch = user.password === password ? true : false ;
+      if (!isPasswordMatch) {
+        return done(null, false , { message : 'password incorrect'});
+        }
+        return done(null, user);
+        
+    } catch (error) {
+      return done(error);
+    }
+   }));
+
+   module.exports = passport;
