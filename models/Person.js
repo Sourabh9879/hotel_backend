4ef4398d532701be9469964aeef3e3ebb05a8548
Sourabh9879 <kumarsourabh970@gmail.com>
2024-08-02 17:31:01 +0530

    password hassing using bcrypt

--- models/Person.js
+++ models/Person.js
@@ -1,43 +1,70 @@
 const mongoose = require('mongoose')
+const bycrypt = require('bcrypt');
 
 const personSchema = new mongoose.Schema({
-    name:{
-        type:String,
-        required:true
+    name: {
+        type: String,
+        required: true
     },
-    age:{
-        type:Number
+    age: {
+        type: Number
     },
-    work:{
-        type:String,
-        enum:['chef','waiter','manager'],
-        required:true
+    work: {
+        type: String,
+        enum: ['chef', 'waiter', 'manager'],
+        required: true
     },
-    mobile:{
-        type:String,
-        required:true
+    mobile: {
+        type: String,
+        required: true
     },
-    email:{
-        type:String,
-        required:true,
-        unique:true
-    }, 
-    address:{
-        type:String
+    email: {
+        type: String,
+        required: true,
+        unique: true
     },
-    salary:{
-        type:Number,
-        required:true
+    address: {
+        type: String
     },
-    username:{
-        required:true,
-        type:String
+    salary: {
+        type: Number,
+        required: true
     },
-    password:{
-        required:true,
-        type:String
+    username: {
+        required: true,
+        type: String
     },
-})
+    password: {
+        required: true,
+        type: String
+    },
+});
 
-const Person = mongoose.model('mongoose',personSchema);
+personSchema.pre('save', async function (next) {
+
+    const person = this;
+    if (!person.isModified('password')) next();
+
+    try {
+        const salt = await bycrypt.genSalt(10);
+        const hashedPassword = await bycrypt.hash(person.password, salt)
+        person.password = hashedPassword;
+        next();
+
+    } catch (error) {
+        return next(error);
+    }
+});
+
+personSchema.methods.comparePassword = async function (candidatePassword){
+    try {
+        const isMatch = await bycrypt.compare(candidatePassword,this.password);
+        return isMatch;
+    } catch (error) {
+        
+    }
+}
+
+
+const Person = mongoose.model('mongoose', personSchema);
 module.exports = Person;
